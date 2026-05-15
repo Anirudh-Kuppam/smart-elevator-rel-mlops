3468a41d6779dc3c596e127df91538a3f05058a2
Channabasavannaa <158754343+Channabasavannaa@users.noreply.github.com>
2026-05-15 23:41:15 +0530

    Integrate dotenv for MLflow tracking configuration
    Added dotenv support for loading environment variables and set MLflow tracking URI.

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,10 @@
 
 import pickle
 
+from dotenv import load_dotenv
 import mlflow
+load_dotenv()
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "mlruns"))
 
 from sim.elevator_env import ElevatorEnv
 
