44140bfb3c4e553befa5a0126b3a695ba9c289ef
Channabasavannaa <158754343+Channabasavannaa@users.noreply.github.com>
2026-05-15 23:40:52 +0530

    Refactor baseline.py to include dotenv support

--- baseline.py
+++ baseline.py
@@ -1,7 +1,9 @@
 
 import random
-
+from dotenv import load_dotenv
 import mlflow
+load_dotenv()
+mlflow.set_tracking_uri(os.environ.get("MLFLOW_TRACKING_URI", "mlruns"))
 
 from sim.elevator_env import ElevatorEnv
 
